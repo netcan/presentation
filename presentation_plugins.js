f6bc616842e74b76dd0faad3974a450a01d5b60b
netcan <1469709759@qq.com>
2021-06-18 19:08:42 +0800

    update node_modules

--- presentation_plugins.js
+++ presentation_plugins.js
@@ -1,2 +1,2 @@
-{ src: '../node_modules/reveal.js/plugin/chalkboard/chalkboard.js' },
+{ src: 'https://cdn.jsdelivr.net/npm/reveal.js-plugins@1.0.0/chalkboard/chalkboard.js' },
 { src: 'https://cdn.jsdelivr.net/npm/reveal.js-menu@1.2.0/menu.js' }
